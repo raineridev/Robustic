c2e53e25882ef78b1944a7258ed74c9c1a7b6a69
Marcelo Raineri <raineridev@gmail.com>
2024-09-19 23:07:03 +0100

    add: add ui packeg

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,30 +1,14 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup lang="ts">
+import { Button } from "./components/ui/button"
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1 class="logo">Robustic</h1>
+  <h2 class=""><span>Payments?</span> We can do more for you</h2>
+  <h3>For your business, use something robust</h3>
+  <Button>Get Start</Button>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style lang="scss" scoped>
+
 </style>
